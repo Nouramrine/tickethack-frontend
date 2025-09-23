e82ca4852fcefb58966bedcd956627ffb370a239
Nour <nouramrine@gmail.com>
2025-09-23 15:16:59 +0200

    bookings html et js

---
+++ bookings.js
@@ -0,0 +1,33 @@
+async function leadBooking() {
+  try {
+    const res = await fetch("/bookings");
+    const data = await res.json();
+
+    const container = document.getElementById("bookings-container");
+    container.innerHTML = "";
+
+    if (!data.result || data.bookings.length === 0) {
+      container.innerHTML = `
+        <p>No booking yet.</p>
+        <p>Why not plan a trip?</p>
+        `;
+      return;
+    }
+    const bookingsHTML = data.bookings
+      .map(
+        (b) =>
+          `<div class="booking-card">
+            <strong>${b.departure} ➝ ${b.arrival}</strong><br>
+            Departure: ${b.date}<br>
+            Price: ${b.price} €<br>
+            Time remaining: ${b.waitingTime}
+            </div>`
+      )
+      .join("");
+
+    const message = `<p class="booking-message">Enjoy your travels with Tickethack!</p>`;
+    container.innerHTML = bookingsHTML + message;
+  } catch (err) {
+    console.error(err);
+  }
+}
